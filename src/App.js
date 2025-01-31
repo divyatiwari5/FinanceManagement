d80cd990280be1d66d88e3a3468b35c7a27fc728
Divya Tiwari <me@divyatiwari.me>
2025-01-31 23:34:15 +0530

    Setup project; font family; tailwind

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import Header from "./components/Header.tsx";
+import { NavBar } from "./components/NavBar/NavBar.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.tsx";
+import Settings from "./pages/Settings.tsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <div className="flex">
+          <div className="w-64">
+            <NavBar />
+          </div>
+          <div className="flex-1">
+            <Header />
+            <Routes>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
