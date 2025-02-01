d591201addb37364857baf3f6d6f8915911593e7
Divya Tiwari <me@divyatiwari.me>
2025-02-01 13:31:52 +0530

    Added Search bar

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,16 @@
+import { IoSearchOutline } from 'react-icons/io5';
+
+const SearchBar = () => {
+  return (
+    <div className="relative flex items-center bg-gray-50 rounded-3xl pl-4">
+      <IoSearchOutline />
+      <input
+        type="text"
+        placeholder="Search for something"
+        className="w-[300px] py-2 pl-4 pr-4 bg-gray-50 text-sm focus:outline-none rounded-3xl"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
