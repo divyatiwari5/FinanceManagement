096db13fe37e0a55d7744ca2833f0f98043f795a
Divya Tiwari <me@divyatiwari.me>
2025-02-01 15:01:42 +0530

    Define colors

---
+++ style/global/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    primaryIndigo: '#343C6A',
+    lightBlue :'#F5F7FA',
+    primaryBlack: '#232323',
+    primaryBlue: '#718EBF',
+    red: '#FF4B4A',
+    green: '#41D4A8',
+}
